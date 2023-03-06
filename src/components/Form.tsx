35d69af902c1dd380b3c6a53f164faa0b75100b5
Taku Inoue <takuinoue0823@gamil.com>
2023-03-06 15:41:57 +0900

    create Tags.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import Tags from '../components/Tags';
 import { StartOptionsContext } from '../context/StartOptionsContext';
 import useInput from '../hooks/useInput';
 import styled from 'styled-components';
@@ -56,70 +57,9 @@ const AmidaButton = styled.button`
   }
 `;
 
-const AmidaTags = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  gap: 16px 16px;
-`;
-
-const AmidaTag = styled.li`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  height: 32px;
-  padding: 0 16px;
-  border: 1px solid #e5e5e5;
-  border-radius: 16px;
-  white-space: nowrap;
-  overflow: hidden;
-`;
-
-const AmidaDelete = styled.span`
-  content: '';
-  position: absolute;
-  top: 50%;
-  right: 8px;
-  transform: translateY(-50%);
-  display: block;
-  width: 20px;
-  height: 20px;
-  background: #fff;
-  border: 1px solid #e5e5e5;
-  border-radius: 50%;
-  transition: all 0.32s;
-  z-index: 10;
-  cursor: pointer;
-  &:hover {
-    background: #fa243c;
-    border-color: #fa243c;
-  }
-  &:hover::before,
-  &:hover::after {
-    background: #fff;
-  }
-  &::before,
-  &::after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 1px;
-    height: 12px;
-    background: #767676;
-    border-radius: 50%;
-  }
-  &::before {
-    transform: translate(-50%, -50%) rotate(45deg);
-  }
-  &::after {
-    transform: translate(-50%, -50%) rotate(-45deg);
-  }
-`;
-
 const Form = () => {
   const startInput = useInput('');
-  const { startOptions, setStartOptions } = useContext(StartOptionsContext);
+  const { setStartOptions } = useContext(StartOptionsContext);
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.nativeEvent.isComposing || event.key !== 'Enter') return;
@@ -142,10 +82,6 @@ const Form = () => {
     setStartOptions([]);
   };
 
-  const deleteTag = (target: string) => {
-    setStartOptions(startOptions.filter((option) => option !== target));
-  };
-
   return (
     <AmidaForm>
       <AmidaInputWrapper>
@@ -161,14 +97,7 @@ const Form = () => {
         <AmidaButton onClick={addStartOptions}>追加</AmidaButton>
         <AmidaButton onClick={clearAllOptions}>クリア</AmidaButton>
       </AmidaInputWrapper>
-      <AmidaTags>
-        {startOptions.map((option) => (
-          <AmidaTag key={option}>
-            {option}
-            <AmidaDelete onClick={() => deleteTag(option)} />
-          </AmidaTag>
-        ))}
-      </AmidaTags>
+      <Tags />
     </AmidaForm>
   );
 };
